86dffc3fb4c413745f8ea2a6ae0ec07327a2b4ab
merdie <merdiemlb@gmail.com>
2021-10-25 14:22:18 +0100

    feat: add others tests components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
+import Message from "./pages/messages";
 import Product from "./pages/Product";
 import Report from "./pages/Report";
+import Support from "./pages/Support";
+import Team from "./pages/Team";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/products" exact component={Product} />
         <Route path="/reports" exact component={Report} />
+        <Route path="/team" exact component={Team} />
+        <Route path="/messages" exact component={Message} />
+        <Route path="/support" exact component={Support} />
       </Switch>
     </BrowserRouter>
   );
